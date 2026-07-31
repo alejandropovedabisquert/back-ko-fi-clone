ecc7f7c02d604ff6502fb16ff997bbdc7595eee1
Alejandro <alejandropovedabisquert@gmail.com>
2026-07-31 16:46:16 +0200

    feat(middleware): implement protection to all api routes by api key

---
+++ app/Http/Middleware/CheckApiKey.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckApiKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKey = $request->header('X-API-KEY');
+        if (!$apiKey || $apiKey !== config('app.api_key')){
+            return response()->json([
+                'message' => 'Unauthorized access.'
+            ], 401);
+        }
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,20 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "Ok";
+// VALIDATE API KEY FROM FRONT
+Route::middleware(['api.key', 'throttle:60,1'])->group(function(){
+    Route::get('/', function () {
+        return "Ok";
+    });
+    
+    // AUTH ROUTES
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    });
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 });
-
-// AUTH ROUTES
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-});
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
