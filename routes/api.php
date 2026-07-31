758a02aecce439557ed19938b780efbdd7cc982c
Alejandro <alejandropovedabisquert@gmail.com>
2026-07-31 17:17:39 +0200

    fix(auth,routes): fix of authenticated routes

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,28 @@ use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// VALIDATE API KEY FROM FRONT
-Route::middleware(['api.key', 'throttle:60,1'])->group(function(){
+// PROTECTED BY API KEY
+Route::middleware(['api.key', 'throttle:60,1'])->group(function () {
     Route::get('/', function () {
         return "Ok";
     });
-    
+
     // AUTH ROUTES
     Route::prefix('auth')->group(function () {
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
-        Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+        // PROTECTED BY AUTH SANCTUM
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
     });
-    
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
+
+    // PROTECTED BY AUTH SANCTUM
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+
 });
